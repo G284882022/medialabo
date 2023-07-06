7728804d7a59c02e6514ba6cc72cb20a1f704d77
G284882022 <228488@st.takushoku-u.ac.jp>
2023-07-06 16:05:26 +0900

    7/6

--- kazuate.js
+++ kazuate.js
@@ -2,26 +2,30 @@
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
+let b1 = document.querySelector('button#kaitou');
+b1.addEventListener('click', hantei);
+
 // 入力回数（予想回数）
 let kaisu = 0;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-// hantei();
-// hantei();
 // hantei();
 
+
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
+function hantei(event) {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let i = document.querySelector('input[name="suuji"]')
+  let yoso = i.value;
   
-  // 課題3-1: 正解判定する
+  let b1 = event.target;
+  // let p1 = document.createElement('p');
+  // p1.setAttribute('span','kaisu');
+  // b1.insertAdjacentElement('afterend', p1);
+  
   // kotae と yoso が一致するかどうか調べて結果を出力
-  
-  // 課題3-1における出力先はコンソール
   kaisu++;
   if(kaisu>3){
     let p = document.querySelectorAll('p');
@@ -29,29 +33,24 @@ function hantei() {
     let result = document.querySelector('p#result');
     result.textContent = '答えは'+ kotae+' でした．すでにゲームは終わっています';
 
-    console.log('答えは'+ kotae+' でした．すでにゲームは終わっています'); //console
   }else{
     let kaisuH = document.querySelector('span#kaisu');
     kaisuH.textContent =kaisu;
     let answerH = document.querySelector('span#answer');
     answerH.textContent = yoso;
 
-    console.log(kaisu+'回目の予想:'+yoso); //console
     if(kotae===yoso){
       let result = document.querySelector('p#result');
       result.textContent = '正解です．おめでとう!';
 
-      console.log('正解です．おめでとう!'); //console
     }else if(kotae>=yoso){
       let result = document.querySelector('p#result');
       result.textContent = 'まちがい．答えはもっと大きいですよ';
 
-      console.log('まちがい．答えはもっと大きいですよ'); //console
     }else{
       let result = document.querySelector('p#result');
       result.textContent = 'まちがい．答えはもっと小さいですよ';
 
-      console.log('まちがい．答えはもっと小さいですよ'); //console
     }
   }
   
